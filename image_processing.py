fc8c69d305567aa18c31e1d106c3a0d8d20d893e
katelyn-zhao <146498901+katelyn-zhao@users.noreply.github.com>
2024-04-03 12:46:28 -0700

    Add files via upload

--- image_processing.py
+++ image_processing.py
@@ -1,101 +1,91 @@
-import os, glob
-import nibabel as nib
-import numpy as np
-import matplotlib.pyplot as plt
+import os
+import io
+import random
+import nibabel
 import cv2
+from scipy.ndimage import rotate
+import numpy as np
+import nibabel as nib
+from nibabel import load
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from PIL import Image
+from keras.utils import Sequence
+from IPython.display import Image, display
+from skimage.exposure import rescale_intensity
+from skimage.segmentation import mark_boundaries
+from keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard
+from keras.utils import normalize
+from sklearn.model_selection import train_test_split
+from keras.callbacks import Callback
+from sklearn.model_selection import KFold
+from keras.models import Model
+from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate, Conv2DTranspose, BatchNormalization, Dropout, Lambda
 
-# CONSTANTS!!!
-
-# STEP 1 - Load and visualize data
-dataInputPath = 'MRI/'
-imagePathInput = os.path.join(dataInputPath, 'Anatomical_mag_echo5/')
-maskPathInput = os.path.join(dataInputPath, 'whole_liver_segmentation/')
-
-dataOutputPath = 'MRI/slices/'
-imageSliceOutput = os.path.join(dataOutputPath, 'img/')
-maskSliceOutput = os.path.join(dataOutputPath, 'mask/')
-
-# STEP 2 - Image normalization
-HOUNSFIELD_MIN = 0
-HOUNSFIELD_MAX = 600
-HOUNSFIELD_RANGE = HOUNSFIELD_MAX - HOUNSFIELD_MIN
-
-# STEP 3 - Slicing and saving
-SLICE_X = False
-SLICE_Y = False
-SLICE_Z = True
-
-SLICE_DECIMATE_IDENTIFIER = 2
 
 image_directory = 'MRI/Anatomical_mag_echo5/'
 mask_directory = 'MRI/whole_liver_segmentation/'
 
-data_output_path = 'MRI/slices/'
-image_slice_output = os.path.join(data_output_path, 'img/')
-mask_slice_output = os.path.join(data_output_path, 'mask/')
+image_dataset = []  
+mask_dataset = []
+sliced_image_dataset = []
+sliced_mask_dataset = []
 
-image_dataset = []  #Many ways to handle data, you can use pandas. Here, we are using a list format.  
-mask_dataset = []  #Place holders to define add labels. We will add 0 to all parasitized images and 1 to uninfected.
+# SIZE = 128
 
 images = os.listdir(image_directory)
-for i, image_name in enumerate(images):    #Remember enumerate method adds a counter and returns the enumerate object
+for i, image_name in enumerate(images):    
     if (image_name.split('.')[1] == 'nii'):
         image = nib.load(image_directory+image_name)
         image = np.array(image.get_fdata())
-        image_dataset.append(image)
+        #image = resize(image, (SIZE, SIZE))
+        image_dataset.append(np.array(image))
         
-#Iterate through all images in Uninfected folder, resize to 64 x 64
-#Then save into the same numpy array 'dataset' but with label 1
-
 masks = os.listdir(mask_directory)
 for i, image_name in enumerate(masks):
     if (image_name.split('.')[1] == 'nii'):
         image = nib.load(mask_directory+image_name)
         image = np.array(image.get_fdata())
-        mask_dataset.append(image)
+        #image = resize(image, (SIZE, SIZE))
+        mask_dataset.append(np.array(image))
 
-# Normalize image
-def normalizeImageIntensityRange(img):
-    img[img < HOUNSFIELD_MIN] = HOUNSFIELD_MIN
-    img[img > HOUNSFIELD_MAX] = HOUNSFIELD_MAX
-    return (img - HOUNSFIELD_MIN) / HOUNSFIELD_RANGE
-
-# Save volume slice to file
-def saveSlice(img, fname, path):
-    img = np.uint8(img * 255)
-    fout = os.path.join(path, f'{fname}.png')
-    cv2.imwrite(fout, img)
-    print(f'[+] Slice saved: {fout}', end='\r')
-
-
-# Slice image in all directions and save
-def sliceAndSaveVolumeImage(vol, fname, path):
-    (dimx, dimy, dimz) = vol.shape
-    print(dimx, dimy, dimz)
-    cnt = 0
-    if SLICE_X:
-        cnt += dimx
-        print('Slicing X: ')
-        for i in range(dimx):
-            saveSlice(vol[i,:,:], fname+f'-slice{str(i).zfill(SLICE_DECIMATE_IDENTIFIER)}_x', path)
-            
-    if SLICE_Y:
-        cnt += dimy
-        print('Slicing Y: ')
-        for i in range(dimy):
-            saveSlice(vol[:,i,:], fname+f'-slice{str(i).zfill(SLICE_DECIMATE_IDENTIFIER)}_y', path)
-            
-    if SLICE_Z:
-        cnt += dimz
-        print('Slicing Z: ')
-        for i in range(dimz):
-            saveSlice(vol[:,:,i], fname+f'-slice{str(i).zfill(SLICE_DECIMATE_IDENTIFIER)}_z', path)
-    return cnt
-
-# Read and process image volumes
 for i in range(len(image_dataset)):
-    numOfSlices = sliceAndSaveVolumeImage(normalizeImageIntensityRange(image_dataset[i]), 'liver'+str(i), imageSliceOutput)
+    for j in range(image_dataset[i].shape[2]):
+        sliced_image_dataset.append(image_dataset[i][:,:,j])
+        sliced_mask_dataset.append(mask_dataset[i][:,:,j])
+        #Adding randomly rotated slices
+        cw = random.randint(0,1)
+        angle = random.randint(5, 10)
+        if cw:
+            sliced_image_dataset.append(rotate(image_dataset[i][:,:,j], angle, reshape = False))
+            sliced_mask_dataset.append(rotate(mask_dataset[i][:,:,j], angle, reshape = False))
+        else:
+            sliced_image_dataset.append(rotate(image_dataset[i][:,:,j], angle * -1, reshape = False))
+            sliced_mask_dataset.append(rotate(mask_dataset[i][:,:,j], angle * -1, reshape = False))
+        #contrast adjustment
+        adjust = random.randint(0,1)
+        contrast = random.randint(1, 2)
+        if adjust:
+            sliced_image_dataset.append(cv2.convertScaleAbs(image_dataset[i][:,:,j], alpha = contrast, beta = 0))
+            sliced_mask_dataset.append(cv2.convertScaleAbs(mask_dataset[i][:,:,j], alpha = contrast, beta = 0))
+        if adjust and cw:
+            sliced_image_dataset.append(rotate(cv2.convertScaleAbs(image_dataset[i][:,:,j], alpha = contrast, beta = 0), angle, reshape = False))
+            sliced_mask_dataset.append(rotate(cv2.convertScaleAbs(mask_dataset[i][:,:,j], alpha = contrast, beta = 0), angle, reshape = False))
 
-# Read and process image mask volumes
-for i in range(len(mask_dataset)):
-    numOfSlices = sliceAndSaveVolumeImage(mask_dataset[i], 'liver'+str(i), maskSliceOutput)
+#Normalize images
+sliced_image_dataset = np.expand_dims(np.array(sliced_image_dataset),3)
+#D not normalize masks, just rescale to 0 to 1.
+sliced_mask_dataset = np.expand_dims((np.array(sliced_mask_dataset)),3)
+
+print(len(sliced_image_dataset))
+
+X_train, X_test, y_train, y_test = train_test_split(sliced_image_dataset, sliced_mask_dataset, test_size = 0.20, random_state = 0)
+
+#Sanity check, view a few images
+image_number = random.randint(0, len(X_train))
+plt.figure(figsize=(12, 6))
+plt.subplot(121)
+plt.imshow(X_train[image_number], cmap='gray')
+plt.subplot(122)
+plt.imshow(y_train[image_number], cmap='gray')
+plt.show()
